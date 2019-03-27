a239ac95b12d817609cf70753743ab31c52df6a8
CHENGE6 <CHENGE6@CHENGE6-W10.corp.oocl.com>
2019-03-27 16:03:31 +0800

    1. service unit test with mock in provider1 & provider2 2. controller & dao not mock in provider1 & provider2 & consumer

---
+++ spring-cloud-provider/src/test/java/com/com/oocl/spring/cloud/provider/dao/UserDaoImplTest.java
@@ -0,0 +1,89 @@
+package com.com.oocl.spring.cloud.provider.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.oocl.spring.cloud.provider.dao.UserDao;
+import com.oocl.spring.cloud.provider.model.User;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UserDaoImplTest {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Value("${all.count}")
+	private int allCount;
+	
+	@Test
+	public void insertUserTest() {
+		User user = new User("chenge6@oocl.com", "gentle", "male");
+		User result = userDao.insertUser(user);
+		assertEquals(user, result);
+	}
+	
+	@Test
+	public void should_return_all_user_when_select_all_user() {
+		List<User> users = userDao.getAllUser();
+		assertEquals(allCount, users.size());
+	}
+	
+	@Test
+	public void should_return_user_count_when_input_right_name_for_select() {
+		String name = "gentle";
+		User user = userDao.getUserByName(name);
+		assertEquals(name, user.getName());
+	}
+	
+	@Test
+	public void should_return_update_count_when_input_right_name_for_update() {
+		String name = "gentle";
+		User user = new User("chenge@oocl.com", "gentle", "male");
+		int result = userDao.updateUserByName(name, user);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void should_return_user_delete_count_when_input_right_name_for_delete() {
+		String name = "gentle";
+		int result = userDao.deleteUserByName(name);
+		assertEquals(1, result);
+	}
+	
+	
+	/*
+	 * exception test
+	 */
+	@Test(expected = NullPointerException.class)
+	public void should_throw_null_exception_when_input_wrong_name_for_update() {
+		String name = "gentle";
+		User user = new User("chenge@oocl.com", "gentle", "male");
+		userDao.updateUserByName(name, user);
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void should_throw_null_exception_when_input_wrong_name_for_select() {
+		String name = "gentle";
+		userDao.getUserByName(name);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void should_throw_null_exception_when_input_wrong_name_for_delete() {
+		String name = "gentle";
+		userDao.deleteUserByName(name);
+	}
+
+}
---
+++ spring-cloud-provider2/src/test/java/com/com/oocl/spring/cloud/provider/controller/UserControllerTest.java
@@ -0,0 +1,97 @@
+package com.com.oocl.spring.cloud.provider.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserControllerTest {
+
+	private MockMvc mvc;
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	@Before
+	public void setup() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void should_return_user_name_when_input_user_for_insert() throws Exception {
+
+		String JsonString = "{\"account\": \"gentle@oocl.com\",\"gender\": \"male\",\"name\": \"gentle\"}";
+		MvcResult mvcResult = mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(JsonString))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("gentle"));
+	}
+
+	@Test
+	public void should_return_error_msg_when_input_wrong_name_for_select() throws Exception {
+		String name = "unRegistered";
+		MvcResult mvcResult = mvc.perform(get("/user/"+name+"")).andExpect(status().isOk()).andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("fail"));
+	}
+	
+	@Test
+	public void should_return_user_when_input_right_name_for_select() throws Exception {
+		String name = "gentle";
+		MvcResult mvcResult = mvc.perform(get("/user/"+name+"")).andExpect(status().isOk()).andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("gentle"));
+	}
+	
+	@Test
+	public void should_return_error_msg_when_input_wrong_name_for_delete() throws Exception {
+		String name = "unRegistered";
+		MvcResult mvcResult = mvc.perform(delete("/user/"+name+"")).andExpect(status().isOk()).andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("fail"));
+	}
+
+	@Test
+	public void should_return_error_msg_when_input_wrong_name_for_update() throws Exception {
+		String name = "gentle";
+		String JsonString = "{\"account\": \"gentle1@oocl.com\",\"gender\": \"male\",\"name\": \"gentle\"}";
+		MvcResult mvcResult = mvc.perform(put("/user/"+name+"").contentType(MediaType.APPLICATION_JSON).content(JsonString))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("\"updateCount\":1"));
+	}
+
+	@Test
+	public void should_return_delete_count_when_input_right_name_for_delete() throws Exception {
+		String name = "gentle";
+		MvcResult mvcResult = mvc.perform(delete("/user/"+name+"")).andExpect(status().isOk()).andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(result.contains("\"updateCount\":1"));
+	}
+	
+	@Test
+	public void should_return_all_user_when_select_all_user() throws Exception {
+		MvcResult mvcResult = mvc.perform(get("/users")).andExpect(status().isOk()).andReturn();
+		String result = mvcResult.getResponse().getContentAsString();
+		Assert.assertNotNull(result);
+	}
+}
